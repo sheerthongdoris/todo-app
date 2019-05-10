c6ea9fd3db52101916949646ebbb3dcc80f06906
sheerthongdoris <sheer.thong.doris@gmail.com>
2019-05-10 11:34:41 +0900

    TODO完了機能を実装しました On branch implement-done-todo

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ $("#add").click(function (){
     var inputTodo = $("#input-todo").val();
     $("#todo-list").append("<li>" + inputTodo + "</li>");
 });
+$(document).on("change","input[type=checkbox]",function (){
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration","line-through");
+    }
+    else{
+        $(this).parent().css("text-decoration","none");
+    }
+});
+
